90fc5efb72c6763b38c25f3ad6af4ba39cddb1d8
PrimeNoot <84326017+PrimeNoot@users.noreply.github.com>
2022-02-01 20:37:57 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,13 @@
+function changetoRed()
+{
+    document.body.style.backgroundColor = "red";
+}
+function changeToGreen()
+{
+    document.body.style.backgroundColor = "green";
+}
+function changeToViolet() 
+{
+    document.body.style.backgroundColor = "purple";
+}
+  
